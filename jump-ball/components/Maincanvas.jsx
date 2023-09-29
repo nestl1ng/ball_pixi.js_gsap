9b9aad309bc269fec3b66afd9becf672954214ec
Eduard Gaidukevich <elladiy2000@yandex.ru>
2023-09-29 12:14:42 +0300

    version_1.3
    version_1.3

--- jump-ball/components/Maincanvas.jsx
+++ jump-ball/components/Maincanvas.jsx
@@ -5,27 +5,55 @@ import * as PIXI from 'pixi.js';
 // import { gsap } from "gsap/dist/gsap";
 
 export default function Maincanvas() {
+  class CanvasBall {
+    constructor(app, ref) {
+      this.ref = ref
+      this.app = app;
+      this.frame;
+      this.ball;
+    }
+
+    paintCanvas() {
+      this.ref.current.appendChild(this.app.view);
+    }
+
+    paintFrame(frame) {
+      if (typeof this.app !== 'undefined') {
+        this.frame = frame;
+        this.frame.beginFill(0x666666);
+        this.frame.drawRect(0, 0, 800, 800);
+        this.frame.position.set(4, 4);
+        this.app.stage.addChild(this.frame);
+      }
+    }
+
+    paintBall(ball) {
+      if (typeof this.frame !== 'undefined') {
+        this.ball = ball;
+        this.ball.width = 50;
+        this.ball.height = 50;
+        this.ball.x = 0;
+        this.ball.y = 750;
+
+        this.frame.eventMode = 'static';
+        this.frame.cursor = 'pointer';
+        this.frame.addChild(this.ball);
+      }
+    }
+  }
+  
   const app = new PIXI.Application({ width: 808, height: 808 });
   const canvasDiv = useRef();
+  let ball = new CanvasBall(app, canvasDiv);
 
   useEffect(() => {
-    canvasDiv.current.appendChild(app.view);
+    ball.paintCanvas();
 
     let frame = new PIXI.Graphics();
-    frame.beginFill(0x666666);
-    frame.drawRect(0, 0, 800, 800);
-    frame.position.set(4, 4);
-    app.stage.addChild(frame);
+    ball.paintFrame(frame);
 
-    let ball = PIXI.Sprite.from(BallImg.src);
-    ball.width = 50;
-    ball.height = 50;
-    ball.x = 0;
-    ball.y = 750;
-
-    frame.eventMode = 'static';
-    frame.cursor = 'pointer';
-    frame.addChild(ball);
+    let ballSprite = PIXI.Sprite.from(BallImg.src);
+    ball.paintBall(ballSprite);
   }, [])
 
   return (
